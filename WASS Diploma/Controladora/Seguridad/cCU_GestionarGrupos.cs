a4e9127d6435399f2f75b49ef40fb74aca6eae47
dantearrighi <dantea@fibertel.com.ar>
2015-10-29 18:16:49 -0300

    Controladora CU Gestionar Perfiles + CU Gestionar Grupos
    Se añadieron 2 clases a controladora

---
+++ WASS Diploma/Controladora/Seguridad/cCU_GestionarGrupos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Controladora.Seguridad
+{
+    public class cCU_GestionarGrupos
+    {
+        // Declaración de variables a usar en la clase
+        private static cCU_GestionarGrupos instancia;
+        private Modelo_Entidades.WASSTDEntidades oModelo_Entidades;
+
+
+        //Aplico el patron de diseño Singleton para la clase controladora del CU Gestionar GRUPOS (cuando la solicitan desde otra)
+        public static cCU_GestionarGrupos ObtenerInstancia()
+        {
+            if (instancia == null)
+                instancia = new cCU_GestionarGrupos();
+
+            return instancia;
+        }
+
+        // Coloco al constructor como privado.
+        private cCU_GestionarGrupos()
+        {
+            oModelo_Entidades = Modelo_Entidades.WASSTDEntidades.ObtenerInstancia();
+        }
+
+
+
+
+
+
+
+    }
+}
